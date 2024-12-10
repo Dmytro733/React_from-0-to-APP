298f1c253c79e38144b1d5cd6fcf98441bf2df7d
Dmytro733 <ddp0496@gmail.com>
2024-12-10 20:35:42 +0200

    Section 6: Styling React Components(Tailwind:NediaQuery/PseudoSelectors/Dynamic Styling with Tailwind): Done

--- Section-6-styling-react-components/src/components/AuthInputs.jsx
+++ Section-6-styling-react-components/src/components/AuthInputs.jsx
@@ -1,7 +1,15 @@
 import { useState } from 'react';
+import { styled } from 'styled-components'
 
-import { StyledControlContainer, TextButton, RectangleButton } from './general-styled-components';
 import Input from './Input';
+import Button from './Button';
+
+const StyledControlContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-bottom: 1.5rem;
+`
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -29,22 +37,22 @@ export default function AuthInputs() {
         <Input
           label="Email"
           type="email"
-          $invalid={emailNotValid}
+          invalid={emailNotValid}
           onChange={(event) => handleInputChange('email', event.target.value)}
         />
 
         <Input
           label="Password"
           type="password"
-          $invalid={passwordNotValid}
+          invalid={passwordNotValid}
           onChange={(event) => handleInputChange('password', event.target.value)}
         />
       </StyledControlContainer>
       <div className="actions">
-        <TextButton type="button" className="text-button">
+        <Button type="button" className="text-button">
           Create a new account
-        </TextButton>
-        <RectangleButton className='button' onClick={handleLogin}>Sign In</RectangleButton>
+        </Button>
+        <Button className='button' onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
