d6854bd413586250ce2f3b1ad2b5a12695f98cf5
Dmytro733 <ddp0496@gmail.com>
2024-12-22 22:47:04 +0200

    Section 9: Splitting Components/ Managing State to Switch Between Components

--- Section-9-project-management-app/src/App.jsx
+++ Section-9-project-management-app/src/App.jsx
@@ -1,11 +1,36 @@
+import { useState } from "react";
+
 import ProjectsSidebar from "./assets/components/ProjectsSidebar";
 import NewProject from "./assets/components/NewProject";
+import NoProjectSelected from "./assets/components/NoProjectSelected";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function handleStartAddProject(){
+    setProjectsState(prevState => {
+      return{
+        ...prevState,
+        selectedProjectId: 'create'
+      }
+    })
+  }
+
+  let contnet;
+
+  if(projectsState.selectedProjectId === undefined){
+    contnet = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+  }else if(projectsState.selectedProjectId === 'create'){
+    contnet = <NewProject />
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NewProject />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {contnet}
     </main>
   );
 }
