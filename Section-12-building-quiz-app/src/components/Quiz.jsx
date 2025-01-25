04dc46e9a291a8987f5520cad83e6d73cc3cbedc
Dmytro733 <ddp0496@gmail.com>
2025-01-25 19:07:56 +0200

    Section 12: 194-196: Outputting Questions/ Shuffling Answers/ Qoestions Timer: Done

--- Section-12-building-quiz-app/src/components/Quiz.jsx
+++ Section-12-building-quiz-app/src/components/Quiz.jsx
@@ -1,8 +1,50 @@
 import { useState } from "react";
 
-export default function Quiz() {
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
-  const [userAnswers, setUserAnswers] = useState(0);
+import QuestionTimer from "./QuestionTimer.jsx";
+import QUESTIONS from "../questions.js";
+import quizCompletedImg from "../assets/quiz-complete.png";
 
-  return <p>Currently active Question</p>
+export default function Quiz() {
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  const activeQuestionIndex = userAnswers.length;
+  const quizIsCompleted = userAnswers.length === QUESTIONS.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer]
+    });
+  }
+
+  if (quizIsCompleted) {
+    return (
+      <div id="summary">
+        <img src={quizCompletedImg} alt="quiz completed" />
+        <h2>Quiz completed!</h2>
+      </div>
+    )
+  }
+
+  const shuffuldAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffuldAnswers.sort(() => Math.random() - 0.5);
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <QuestionTimer 
+          timeout={10000} 
+          onTimeout={() => handleSelectAnswer(null)} 
+        />
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <div id="answers">
+          {shuffuldAnswers.map((answer, index) => (
+            <li key={index} className="answer"> 
+              <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+            </li>
+          ))}
+        </div>
+      </div>
+      
+    </div>
+  )
 }
